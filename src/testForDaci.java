4a18aaa78597fd2317287cd14a7174801d5cdd48
Filip Filev <124343742+FilipFilev@users.noreply.github.com>
2023-02-03 16:58:32 +0200

    I do exercises from Lecture 2. Conditional Statements - Exercise

---
+++ src/testForDaci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class testForDaci {
+    public static void main(String[] args) {
+        Scanner ConsoleReader = new Scanner(System.in);
+
+        double inch = Double.parseDouble(ConsoleReader.nextLine());
+
+        double cm = inch * 2.54;
+
+        System.out.println(cm);
+
+    }
+}
+
